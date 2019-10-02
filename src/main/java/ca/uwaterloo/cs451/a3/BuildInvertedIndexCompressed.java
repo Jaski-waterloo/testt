1e91b2cc545187731ef0b3f497c3977f2cdf8a72
Jaski-waterloo <55409139+Jaski-waterloo@users.noreply.github.com>
2019-10-02 13:00:24 -0400

    Update BuildInvertedIndexCompressed.java

--- src/main/java/ca/uwaterloo/cs451/a3/BuildInvertedIndexCompressed.java
+++ src/main/java/ca/uwaterloo/cs451/a3/BuildInvertedIndexCompressed.java
@@ -110,7 +110,7 @@ public class BuildInvertedIndexCompressed extends Configured implements Tool {
 //       context.write(new Text(newKey), new PairOfWritables<>(DF, postings));
     }
 //     @Override
-    public void cleanup(Context context)throws IOException
+    public void cleanup(Context context)throws IOException, InterruptedException
     {
       DF.set(df);
       context.write(new Text(prev), new PairOfWritables<>(DF, postings));
