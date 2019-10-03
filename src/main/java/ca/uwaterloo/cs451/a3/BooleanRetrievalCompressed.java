0203228f605fe6807f28bb500032c45a7c787831
Jaski-waterloo <55409139+Jaski-waterloo@users.noreply.github.com>
2019-10-03 17:11:16 -0400

    Update BooleanRetrievalCompressed.java

--- src/main/java/ca/uwaterloo/cs451/a3/BooleanRetrievalCompressed.java
+++ src/main/java/ca/uwaterloo/cs451/a3/BooleanRetrievalCompressed.java
@@ -20,6 +20,7 @@ import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.conf.Configured;
 import org.apache.hadoop.fs.FSDataInputStream;
 import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.FileStatus;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.BytesWritable;
