eac5ced3b871e8d1f92ff6f9beda3432267bfce1
Jaski-waterloo <55409139+Jaski-waterloo@users.noreply.github.com>
2019-10-03 14:20:30 -0400

    Update BuildInvertedIndexCompressed.java

--- src/main/java/ca/uwaterloo/cs451/a3/BuildInvertedIndexCompressed.java
+++ src/main/java/ca/uwaterloo/cs451/a3/BuildInvertedIndexCompressed.java
@@ -54,7 +54,7 @@ import java.util.Collections;
 import java.util.Iterator;
 import java.util.List;
 
-import java.io.*
+import java.io.*;
 
 public class BuildInvertedIndexCompressed extends Configured implements Tool {
   private static final Logger LOG = Logger.getLogger(BuildInvertedIndexCompressed.class);
